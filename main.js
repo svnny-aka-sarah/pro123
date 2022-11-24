541574cf3ed547c005a8dcbca50ed9e3f59721fa
svnny-aka-sarah <116078227+svnny-aka-sarah@users.noreply.github.com>
2022-11-24 22:24:10 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(535, 500);
+    canvas =createCanvas(550, 550);
+    canvas.position(550, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses()
+{
+    
+}
